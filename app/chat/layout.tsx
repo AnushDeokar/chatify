58876b9f97081ea4f654092dd7f4c0f196447276
AnushDeokar <deokaranush@gmail.com>
2023-09-29 17:21:36 +0530

    layout moved to page

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,38 +1,8 @@
-"use client"
-import DesktopSideBar from "@/components/desktop-sidebar"
-import ChatUsersList from "@/components/chat-users-list"
-import { useEffect, useState } from "react"
-
 export default function layout({children}:{children: React.ReactNode} ){
-
-    useEffect(()=>{
-        console.log("called");
-    }, [])
-
-    const [sidebaroption, setSideBarOption] = useState<Number>(0);
-    const chatlist = [
-        {
-            name:"User123",
-            img:""
-        },
-        {
-            name:"User128",
-            img:""
-        },
-        {
-            name:"User129",
-            img:""
-        },
-
-    ]
 
     return(<>
         <div className="h-screen flex overflow-x-hidden">
-            <DesktopSideBar option={sidebaroption} handleOptionChange={(i:Number)=>setSideBarOption(i)}/>
-            <div className="grow w-full h-screen chat main_chat">
-                <ChatUsersList option={sidebaroption} chatlist={chatlist}/>
-                {children}
-            </div>    
+                {children} 
             {/* <button onClick={()=>signOut({ callbackUrl: '/' })}>Signout</button> */}
         </div>
     </>)
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,18 +1,45 @@
 'use client'
 import { useSession, signOut } from "next-auth/react"
 import {Spinner} from "@nextui-org/react";
+import DesktopSideBar from "@/components/desktop-sidebar";
+import ChatUsersList from "@/components/chat-users-list";
+import { useEffect, useState } from "react"
+
 
 function page() {
     const { data: session, status } = useSession();
 
-    if (status === "loading") {
-        return <Spinner size="lg"/>
-    }
+    useEffect(()=>{
+        console.log("called");
+    }, [])
+
+    const [sidebaroption, setSideBarOption] = useState<Number>(0);
+    const chatlist = [
+        {
+            name:"User123",
+            img:""
+        },
+        {
+            name:"User128",
+            img:""
+        },
+        {
+            name:"User129",
+            img:""
+        },
+
+    ]
 
     return (
-        <div className="w-full hidden lg:block border flex flex-col none lg:border h-full" style={{display:"flex", flexDirection:"column"}}>
-            <h1 className="m-auto text-2xl">Start a new chat or reply to an existing chat🚀</h1>
-        </div>
+        <>
+            <DesktopSideBar option={sidebaroption} handleOptionChange={(i:Number)=>setSideBarOption(i)}/>
+            <div className="grow w-full h-screen chat main_chat">
+                <ChatUsersList option={sidebaroption} chatlist={chatlist}/>
+                <div className="w-full hidden lg:block border flex flex-col none lg:border h-full" style={{display:"flex", flexDirection:"column"}}>
+                <h1 className="m-auto text-2xl">Start a new chat or reply to an existing chat🚀</h1>
+                </div>
+            </div>   
+        </>
     )
 }
 
